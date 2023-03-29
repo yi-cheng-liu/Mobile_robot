2fa2793130ca51fc69e535b0580c2dca3810b0df
yi-cheng-liu <111710682+yi-cheng-liu@users.noreply.github.com>
2023-03-28 20:48:22 -0400

    hw5 EKF

--- hw5-code/HW5_codes_python/filter/EKF.py
+++ hw5-code/HW5_codes_python/filter/EKF.py
@@ -14,13 +14,13 @@ class EKF:
         # Inputs:
         #   system: system and noise models
         #   init:   initial state mean and covariance
-        self.gfun = system.gfun  # motion model
-        self.hfun = system.hfun  # measurement model
-        self.Gfun = init.Gfun  # Jocabian of motion model
-        self.Vfun = init.Vfun  # Jocabian of motion model
-        self.Hfun = init.Hfun  # Jocabian of measurement model
-        self.M = system.M # motion noise covariance
-        self.Q = system.Q # measurement noise covariance
+        self.gfun = system.gfun   # motion model
+        self.hfun = system.hfun   # measurement model
+        self.Gfun = init.Gfun     # Jocabian of motion model
+        self.Vfun = init.Vfun     # Jocabian of motion model
+        self.Hfun = init.Hfun     # Jocabian of measurement model
+        self.M = system.M         # motion noise covariance
+        self.Q = system.Q         # measurement noise covariance
 
         self.state_ = RobotState()
 
@@ -40,7 +40,16 @@ class EKF:
         # TODO: Implement the prediction step for EKF                                 #
         # Hint: save your predicted state and cov as X_pred and P_pred                #
         ###############################################################################
-        
+        # Prediction step
+
+        # 1. State prediction
+        X_pred = self.gfun(X, u)
+
+        # 2. Covariance prediction
+        G_t = self.Gfun(X, u)
+        V_t = self.Vfun(X, u)
+        P_pred = np.dot(np.dot(G_t, P), G_t.T) + np.dot(np.dot(V_t, self.M), V_t.T)
+
 
         ###############################################################################
         #                         END OF YOUR CODE                                    #
@@ -68,6 +77,28 @@ class EKF:
         # Hint: you can use landmark1.getPosition()[0] to get the x position of 1st   #
         #       landmark, and landmark1.getPosition()[1] to get its y position        #
         ###############################################################################
+        # Correction step
+
+        # Measurement prediction
+        z_pred = self.hfun(X_predict, landmark1.getPosition(), landmark2.getPosition())
+
+        # Measurement residual
+        innovation = z - z_pred
+        innovation[1] = wrap2Pi(innovation[1])  # Wrap angle difference to [-pi, pi]
+        innovation[4] = wrap2Pi(innovation[4])  # Wrap angle difference to [-pi, pi]
+
+        # Compute the Jacobian H_t
+        H_t = self.Hfun(X_predict, landmark1.getPosition(), landmark2.getPosition())
+
+        # Kalman gain
+        S = np.dot(np.dot(H_t, P_predict), H_t.T) + self.Q
+        K = np.dot(np.dot(P_predict, H_t.T), np.linalg.inv(S))
+
+        # State update
+        X = X_predict + np.dot(K, innovation)
+
+        # Covariance update
+        P = np.dot((np.eye(3) - np.dot(K, H_t)), P_predict)
 
         
         ###############################################################################
